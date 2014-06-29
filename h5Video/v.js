130928a275e76ed8ebe093193a074c6f40c8d458
Joy Youg <zhou.y.g890@gmail.com>
2014-06-29 22:57:42 +0800

    fix few

--- h5Video/v.js
+++ h5Video/v.js
@@ -1,17 +1,13 @@
 var v = $("video")[0];
-v.src = "Let_it_go.mp4";
 navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-alert(navigator.getUserMedia);
 if (navigator.getUserMedia) {
-
 	function success(stream) {
-		v.src = stream;
+		v.src = URL.createObjectURL(stream);
 	}
 	function error(e) {
 		console.log(e);
 	}
 	var u = navigator.getUserMedia({video:true},success,error);
-
 } else {
-
+	alert("false");
 }
